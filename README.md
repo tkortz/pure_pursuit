92eb1c5207515434fc91405445557066d463c75a
Tanya Amert <tamert@cs.unc.edu>
2021-11-10 20:58:18 -0500

    Updated Pure Pursuit to be a real-time task.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Original Source
 
 This Pure Pursuit implementation was originally part of https://github.com/AtsushiSakai/PythonRobotics.
+
+# Changes
+
+This code has been updated to work with [LITMUS^RT](https://github.com/LITMUS-RT/litmus-rt) and the [Python liblitmus wrapper](https://github.com/tkortz/liblitmus-py), thus enabling it to run as a real-time task.
--- pure_pursuit.py
+++ pure_pursuit.py
@@ -5,10 +5,16 @@ Path tracking simulation with pure pursuit steering and PID speed control.
 author: Atsushi Sakai (@Atsushi_twi)
         Guillaume Jacquenot (@Gjacquenot)
 
+modified by: Tanya Amert (@tkortz)
+
+Original source: https://github.com/AtsushiSakai/PythonRobotics/blob/master/PathTracking/pure_pursuit/pure_pursuit.py
+
 """
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+
+import liblitmus
 
 # Parameters
 k = 0.1  # look forward gain
@@ -17,7 +23,7 @@ Kp = 1.0  # speed proportional gain
 dt = 0.1  # [s] time tick
 WB = 2.9  # [m] wheel base of vehicle
 
-show_animation = True
+show_animation = False
 
 
 class State:
@@ -142,15 +148,60 @@ def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
                   fc=fc, ec=ec, head_width=width, head_length=width)
         plt.plot(x, y)
 
+# https://en.wikipedia.org/wiki/Perlin_noise
+def interpolate(a0, a1, w):
+    return (a1-a0) * (3.0 - w*2.0) * w * w + a0
+
+def randomGradient(ix, iy):
+    r = 2920.0 * math.sin(ix * 21942.0 + iy * 171324.0 + 8912.0) * math.cos(ix * 23157.0 * iy * 217832.0 + 9758.0)
+    return (math.cos(r), math.sin(r))
+
+def dotGridGradient(ix, iy, x, y):
+    g = randomGradient(ix, iy)
+
+    dx = x - ix
+    dy = y - iy
+
+    return (dx * g[0] + dy * g[1])
+
+def perlin(x, y):
+    x0 = int(x)
+    x1 = x0+1
+    y0 = int(y)
+    y1 = y0+1
+
+    sx = x - x0
+    sy = y - y0
+
+    n0 = dotGridGradient(x0, y0, x, y)
+    n1 = dotGridGradient(x1, y0, x, y)
+    ix0 = interpolate(n0, n1, sx)
+
+    n0 = dotGridGradient(x0, y1, x, y)
+    n1 = dotGridGradient(x1, y1, x, y)
+    ix1 = interpolate(n0, n1, sx)
+
+    return interpolate(ix0, ix1, sy)
 
 def main():
+    max_num_jobs = 20000
+    """
+      max_num_jobs = 100    -->   34 jobs
+      max_num_jobs = 1000   -->  203 jobs
+      max_num_jobs = 10000  --> 1883 jobs
+      max_num_jobs = 20000  --> 3750 jobs
+    """
+    T = float(max_num_jobs * dt)
+    xmax = int(T / 2)
+
+    print("Num jobs: {0} (T={1}, xmax={2})".format(max_num_jobs, T, xmax))
+
     #  target course
-    cx = np.arange(0, 50, 0.5)
-    cy = [math.sin(ix / 5.0) * ix / 2.0 for ix in cx]
+    cx = np.arange(0, xmax, 0.5)
+    cy_f = lambda ix: math.sin(ix / 10.0) * (math.sin(ix/5)) * 2.0
+    cy = [cy_f(ix) + 0.05 * perlin(ix, cy_f(ix)) for ix in cx]
 
     target_speed = 10.0 / 3.6  # [m/s]
-
-    T = 100.0  # max simulation time
 
     # initial state
     state = State(x=-0.0, y=-3.0, yaw=0.0, v=0.0)
@@ -162,7 +213,30 @@ def main():
     target_course = TargetCourse(cx, cy)
     target_ind, _ = target_course.search_target_index(state)
 
+    # Set up litmus task
+    wcet = 150
+    period = 200
+    deadline = 200
+    phase = 0
+    early = False
+
+    liblitmus.call_init_litmus()
+    print("\nPure pursuit called init_litmus.\n")
+
+    liblitmus.call_set_rt_task_param(wcet, period, deadline, phase, early)
+    print("\nPure pursuit finished setting rt params.\n")
+
+    liblitmus.set_task_mode_litmusrt()
+    print("\nPure pursuit is now a real-time task.\n")
+
+    print("\nPure pursuit is about to wait for synchronous release.\n")
+    liblitmus.call_wait_for_ts_release()
+
+    num_jobs = 0
+
     while T >= time and lastIndex > target_ind:
+        # Start job
+        print("Starting job {0}".format(time / dt))
 
         # Calc control input
         ai = proportional_control(target_speed, state.v)
@@ -188,6 +262,15 @@ def main():
             plt.grid(True)
             plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
             plt.pause(0.001)
+
+        # End job
+        num_jobs += 1
+        print("Finished PP job #{0}".format(num_jobs))
+        liblitmus.call_sleep_next_period()
+
+    # Clean up litmus task
+    liblitmus.set_task_mode_background()
+    print("\nPure pursuit is now a background task again.\n")
 
     # Test
     assert lastIndex >= target_ind, "Cannot goal"
